8e4b9dcc400ecd7a4ebd2ed8b7760950e80166eb
Iain H <iain@spiralpoint.org>
2023-02-11 17:37:17 -0500

    Output 2018 edition Rust code

--- genRust.go
+++ genRust.go
@@ -109,11 +109,7 @@ func (gen *CodeGenerator) GenRust() error {
 		return err
 	}
 	defer f.Close()
-	var extern = `#[macro_use]
-extern crate serde_derive;
-extern crate serde;
-extern crate serde_xml_rs;
-
+	var extern = `use serde::{Deserialize, Serialize};
 use serde_xml_rs::from_reader;`
 	source := []byte(fmt.Sprintf("%s\n\n%s\n%s", copyright, extern, gen.Field))
 	f.Write(source)
